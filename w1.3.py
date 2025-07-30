9f040922c8d6a10d734786bbc6564578cae35bf6
mythry3 <22eg107a44@anurag.edu.in>
2025-07-30 21:43:18 +0530

    third

--- w1.3.py
+++ w1.3.py
@@ -1,20 +1,20 @@
-#Experiment 2
+#Experiment 3
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 
 
-#dat
+#data
 X = np.linspace(0,1,1000)
 y=5*X+7+np.random.randn(1000)
 
 #build the architecture
-model=Sequential()
+model = Sequential()
 model.add(Dense(units=1, input_shape=(1,), activation='linear'))
 
 #compile
-model.compile(optimizer='sgd',loss='mean_squared_error')
+model.compile(optimizer='adam',loss='mean_squared_error')
  
 #Build a model
 model.fit(X,y,epochs=50)
@@ -22,7 +22,7 @@ model.fit(X,y,epochs=50)
 
 
 
-#visualization
+#visualizatio
 plt.scatter(X,y,label='original data')
-plt.title('Optimizer: SGD, Epoch:50')
+plt.title('Optimizer: Adam')
 plt.show()
